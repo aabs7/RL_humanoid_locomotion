847339a709857b2f9bed28aed0e547e2f76aa788
Abhish Khanal <abheeshkhanal@gmail.com>
2026-08-28 16:40:16 -0700

    default main script

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import gymnasium as gym
+
+
 def main():
-    print("Hello world!")
+    env = gym.wrappers.vector.RecordEpisodeStatistics(gym.make_vec("LunarLander-v3", num_envs=1, render_mode="human"))
+    env.reset()
+
+    for i in range(200):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        print("\nStep:", i)
+        print(f"  obs: {obs} \n"
+              f"  reward: {reward} \n"
+              f"  terminated: {terminated} \n"
+              f"  truncated: {truncated} \n"
+              f"  info: {info}")
+        if all(terminated) or all(truncated):
+            env.reset()
 
 if __name__ == "__main__":
     main()
